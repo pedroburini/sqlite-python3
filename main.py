ec39d27d3f1b73517e93f94e9ae6aadbfcdd96da
Pedro Burini <pulisses10@gmail.com>
2023-09-05 12:49:28 -0300

    insert into, delete without where and sqlite_sequence

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ TABLE_NAME = 'customers'
 connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 
+# create table
 cursor.execute(
     f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
     '('
@@ -17,6 +18,23 @@ cursor.execute(
     'name TEXT,'
     'weight REAL'
     ')'
+)
+connection.commit()
+
+# caution: delete without where
+cursor.execute(
+    f'DELETE FROM {TABLE_NAME}'
+)
+cursor.execute(
+    f'DELETE FROM sqlite_sequence WHERE name="{TABLE_NAME}"'
+)
+connection.commit()
+
+# register values in table columns
+# caution: sql injection
+cursor.execute(
+    f'INSERT INTO {TABLE_NAME} (id, name, weight) '
+    'VALUES (NULL, "a", 1), (NULL, "b", 2)'
 )
 connection.commit()
 
